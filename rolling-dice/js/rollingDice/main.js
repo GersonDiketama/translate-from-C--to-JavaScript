e9a011ea89d9192b0625c5c00e51b78de0228042
GersonGmd <gersondiketama@yahoo.com>
2021-11-15 21:59:38 -0600

    Finished all the exercises

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+let dieString = ["zero", "one", "two", "three", "four", "five", "six"];
+
+for (let i = 0; i < 10; i++) {
+  let dieValue = Math.floor(Math.random() * 6) + 1;
+
+  let dieValue2 = Math.floor(Math.random() * 6) + 1;
+
+  let message = `${dieString[dieValue]} + ${dieString[dieValue2]} == ${
+    dieValue + dieValue2
+  }`;
+
+  if (dieValue == dieValue2) {
+    message += " DOUBLES";
+  }
+  console.log(message);
+}
