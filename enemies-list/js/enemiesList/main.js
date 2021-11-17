e25da4ca84255adfab2b5199233394c1fc0b7fd0
GersonGmd <gersondiketama@yahoo.com>
2021-11-17 14:06:34 -0600

    changes made in enemies-list and grimoire

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,63 @@
 // Put your code here
+console.log("My Enemies List");
+console.log("---------------");
+
+const enemies = [
+  {
+    FirstName: "Joshua",
+    LastName: "Flowers",
+    Offenses: [
+      {
+        offense: "Being a jerk to me in elementary school",
+        offense2: "Not being nice to me in elementary school",
+      },
+    ],
+    isReallyHated: true,
+  },
+
+  {
+    FirstName: "Darth",
+    LastName: "Vader",
+    Offenses: [
+      {
+        offense: "Cut off Luke's hand",
+        offense2: "Murdered all those kids",
+        offense3: "Unkind management practices",
+      },
+    ],
+    isReallyHated: false,
+  },
+
+  {
+    FirstName: "Betty",
+    LastName: "Rudelady",
+    Offenses: [
+      {
+        offense: "Phone calls in the theater",
+        offense2: "Phone calls on the bus",
+        offense3: "Phone calls in line at the grocery store",
+        offense4: "Poor conversationalist",
+      },
+    ],
+    isReallyHated: true,
+  },
+
+  {
+    FirstName: "Leon",
+    LastName: "Peck",
+    Offenses: [
+      {
+        offense: "Keeps giving me a hotplate",
+      },
+    ],
+    isReallyHated: false,
+  },
+];
+
+for (const enemy of enemies) {
+  if (enemy.isReallyHated) {
+    console.log(`${enemy.FirstName} ${enemy.LastName} Really, really dislike!`);
+  } else {
+    console.log(`${enemy.FirstName} ${enemy.LastName}`);
+  }
+}
--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,52 @@
 // Put your code here
+
+console.log("Do you believe in magic?");
+console.log("------------------------");
+
+const allSpells = [
+  {
+    name: "Turn enemy into a newt",
+    isEvil: true,
+    energyRequired: 5.1,
+  },
+
+  {
+    name: "Conjure some gold for a local charity",
+    isEvil: false,
+    energyReqired: 2.99,
+  },
+
+  {
+    name: "Give a deaf person the ability to heal",
+    isEvil: false,
+    energyReqired: 12.2,
+  },
+
+  {
+    name: "Make yourself emperor of the universe",
+    isEvil: true,
+    energyReqired: 100.0,
+  },
+
+  {
+    name: "Convince your relatives your political views are correct",
+    isEvil: false,
+    energyReqired: 2921.5,
+  },
+];
+
+console.log("Good Book");
+
+for (const spells of allSpells) {
+  if (spells.isEvil == false) {
+    console.log(spells.name);
+  }
+}
+console.log();
+console.log("Evil Book");
+
+for (const spells of allSpells) {
+  if (spells.isEvil == true) {
+    console.log(spells.name);
+  }
+}
